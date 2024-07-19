21cf6754994c38cbc2784d2bac8a95e5c415336d
dirunru <15514209607@163.com>
2024-07-19 16:08:43 +0800

    feat：transition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,19 @@
+
+
+<template>
+	<a-space wrap>
+		<!-- <a-button type="primary">Primary Button</a-button>
+		<a-button>Default Button</a-button>
+		<a-button type="dashed">Dashed Button</a-button>
+		<a-button type="text">Text Button</a-button>
+		<a-button type="link">Link Button</a-button>
+		<div class="circle" :style="{ backgroundColor: token.colorPrimary }"></div> -->
+		<button @click="show = !show">Toggle</button>
+	</a-space>
+	<Transition>
+		<p v-if="show">{{ homePage.content }}</p>
+	</Transition>
+</template>
 <script setup>
 import { reactive, ref } from "vue";
 import { theme } from "ant-design-vue";
@@ -6,6 +22,7 @@ const { token } = useToken();
 defineProps({
 	msg: String
 });
+const show = ref(true);
 let homePage = reactive({
 	name: "首页",
 	content: "首页内容",
@@ -13,22 +30,32 @@ let homePage = reactive({
 });
 </script>
 
-<template>
-	<a-space wrap>
-		<a-button type="primary">Primary Button</a-button>
-		<a-button>Default Button</a-button>
-		<a-button type="dashed">Dashed Button</a-button>
-		<a-button type="text">Text Button</a-button>
-		<a-button type="link">Link Button</a-button>
-		<div class="circle" :style="{ backgroundColor: token.colorPrimary }"></div>
-	</a-space>
-</template>
 
 <style scoped>
 .circle {
 	width: 100px;
 	height: 100px;
 	border-radius: 50%;
+}
+/* 下面我们会解释这些 class 是做什么的 */
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+
+.list-enter-active,
+.list-leave-active {
+	transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+	opacity: 0;
+	transform: translateX(30px);
 }
 </style>
 
