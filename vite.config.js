0b22bc952ba82a4cc0e480eb59e46081997c2036
dirunru <15514209607@163.com>
2024-07-24 11:25:11 +0800

    11

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@/": resolve(__dirname, "src") + "/",
+      "@assets/": resolve(__dirname, "assets") + "/",
+      "@utils/": resolve(__dirname, "utils") + "/",
       components: "src/components", // 为components目录设置别名
     },
   },
@@ -30,6 +32,61 @@ export default defineConfig({
     assetsDir: "assets",
     cssCodeSplit: true,
     sourcemap: false,
+  },
+  build: {
+    minify: false,
+    // 进行压缩计算
+    brotliSize: false,
+    //指定输出路径
+    assetsDir: "./",
+    // 指定输出文件路径
+    outDir: "dist",
+    // 代码压缩配置
+    terserOptions: {
+      // 生产环境移除console
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    // chunkSizeWarningLimit: 1500,大文件报警阈值设置,不建议使用
+    rollupOptions: {
+      output: {
+        //静态资源分类打包
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        // assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+        assetFileNames(assetInfo) {
+          // 判断后缀分别放到不用的文件夹中
+          if (assetInfo.name.endsWith(".css")) {
+            return "static/css/[name]-[hash].[ext]";
+          }
+          if (
+            ["png", "jpg", "svg", "PNG"].some((ext) =>
+              assetInfo.name.endsWith(ext)
+            )
+          ) {
+            return "static/img/[name]-[hash].[ext]";
+          }
+          if (
+            ["ttf", "woff", "woff2"].some((ext) => assetInfo.name.endsWith(ext))
+          ) {
+            return "static/fonts/[name]-[hash].[ext]";
+          }
+          return "static/css/[name]-[hash].[ext]";
+        },
+        manualChunks(id) {
+          //静态资源分拆打包
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
   plugins: [
     vue(),
