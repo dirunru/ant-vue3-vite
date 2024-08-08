ccd24fe98d125165c3bd91cc1008fe0d8df64766
dirunru <15514209607@163.com>
2024-08-08 18:35:44 +0800

    feat:11

---
+++ src/components/SelfUseSelf.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="chart-layout">
+    <div class="chart-main">
+      <div>{{ chartData.nameCN }}</div>
+      <div class="chart-en">{{ chartData.nameEN }}</div>
+      <div class="chart-remarks">{{ chartData.remarks }}</div>
+    </div>
+    <SelfItem :chartData="item" v-for="(item, index) in chartData.children" :key="index" />
+  </div>
+</template>
+
+<script setup>
+  import { ref, reactive, defineEmits, defineProps } from 'vue';
+  const emit = defineEmits(['item-click']);
+  const props = defineProps({
+    chartData: {
+      type: Object,
+      default: () => ({})
+    }
+  });
+  const handleItemClick = () => {
+    emit('item-click');
+  };
+</script>
+<script>
+  export default {
+    name: 'SelfItem'
+  };
+</script>
+<style lang="less" scoped>
+  .chart-main {
+    width: 100px;
+    border: 1px solid #000;
+
+    > div {
+      margin-top: 5px;
+    }
+  }
+</style>
--- src/views/Home/Loading.vue
+++ src/views/Home/Loading.vue
@@ -1,33 +1,62 @@
 <template>
-	<div class="s-body">
-		<div class="wrapper">
-			<div class="block">
-				<SLoading />
-			</div>
-		</div>
-	</div>
+  <div class="s-body">
+    <div class="wrapper">
+      <div class="block">
+        <SLoading />
+      </div>
+    </div>
+    <SelfUseSelf :chartData="item" v-for="(item, index) in chartData" :key="index" />
+  </div>
 </template>
 
 <script setup>
-import { ref, reactive } from "vue";
-import SLoading from "@/components/SLoading.vue";
+  import { ref, reactive } from 'vue';
+  import SLoading from '@/components/SLoading.vue';
+  import SelfUseSelf from '@/components/SelfUseSelf.vue';
+
+  const chartData = [
+    {
+      nameCN: '111',
+      nameEN: 'en111',
+      remarks: '我是111',
+      children: [
+        {
+          nameCN: '222',
+          nameEN: 'en222',
+          remarks: '我是222',
+          children: [
+            {
+              nameCN: '222-111',
+              nameEN: 'en222-111',
+              remarks: '我是222-111'
+            }
+          ]
+        },
+        {
+          nameCN: '333',
+          nameEN: 'en333',
+          remarks: '我是333'
+        }
+      ]
+    }
+  ];
 </script>
-<style lang='less' scoped>
-.wrapper {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	height: 100%;
-}
-.block {
-	width: 200px;
-	height: 200px;
-}
+<style lang="less" scoped>
+  .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  .block {
+    width: 200px;
+    height: 200px;
+  }
 </style>
 
 <script>
-// 静态元数据
-export const meta = {
-	title: "loading"
-};
+  // 静态元数据
+  export const meta = {
+    title: 'loading'
+  };
 </script>
