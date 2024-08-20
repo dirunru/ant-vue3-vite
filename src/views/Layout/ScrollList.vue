392976fd825a6820ec621d731d4eef5bc94b4bd2
dirunru <15514209607@163.com>
2024-08-20 13:40:41 +0800

    无限滚动

--- src/views/Layout/ScrollList.vue
+++ src/views/Layout/ScrollList.vue
@@ -1,20 +1,18 @@
 <template>
-  <!-- <div class="scroll_animate">
-    <div class="animate_item">1</div>
-    <div class="animate_item">2</div>
-    <div class="animate_item">3</div>
-    <div class="animate_item">4</div>
-    <div class="animate_item">5</div>
-    <div class="animate_item">6</div>
-    <div class="animate_item">7</div>
-    <div class="animate_item">8</div>
-  </div> -->
-  <a-button @click="add">添加</a-button>
-  <SScrollList :columns="columns" :dataSource="dataSource" :headerHeight="40" :rowHeigh="32" :scroll="true" :height="200"></SScrollList>
+  <div>
+    <pre>
+      setTimeout虽然也可以实现，但是页面来回切换会导致时间混乱
+    </pre>
+    <pre>
+      window.requestAnimationFrame实现
+    </pre>
+    <a-button @click="add">添加</a-button>
+    <SScrollList :columns="columns" :dataSource="dataSource" :headerHeight="40" :rowHeigh="32" :scroll="true" :height="200"></SScrollList>
+  </div>
 </template>
 
 <script setup>
-  import { ref, reactive, onMounted } from 'vue';
+  import { ref, reactive, onMounted, onDeactivated, onUnmounted } from 'vue';
   import SScrollList from '@/components/SScrollList.vue';
   const columns = reactive([
     {
@@ -73,34 +71,6 @@
       address: 'Ottawa No. 2 Lake Park'
     });
   };
-  // function scrollAnimate() {
-  //   let domArray = document.getElementsByClassName('animate_item');
-  //   let domFirst = domArray[0];
-  //   let parentDom = document.getElementsByClassName('scroll_animate')[0];
-  //   parentDom.style.height = 40 * domArray.length + 'px';
-  //   Array.from(parentDom).forEach((item) => {
-  //     item.style.height = '40px';
-  //   });
-  //   domFirst.style.height = '0px';
-  //   // 使用setTimeout来模拟异步操作
-  //   setTimeout(() => {
-  //     // 移除第一个元素
-  //     domFirst.remove();
-  //     // 将第一个元素添加到.scroll_animate容器的末尾
-  //     parentDom.appendChild(domFirst);
-  //     // 设置现在将作为最后一个元素的第一个元素的高度为40
-  //     domFirst.style.height = '40px';
-  //     // 再次使用setTimeout来模拟之后的某个操作，比如执行动画
-  //     setTimeout(() => {
-  //       scrollAnimate(); // 假设scrollAnimate是一个已经定义的函数
-  //     }, 1000);
-  //   }, 250);
-  // }
-  // onMounted(() => {
-  //   setTimeout(() => {
-  //     scrollAnimate();
-  //   }, 1000);
-  // });
 </script>
 <style lang="less" scoped>
   .scroll_animate {
